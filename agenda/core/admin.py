3a033c91c31ed0487f9ba65fd733c2038593598f
Luíza <luizaesteves987@gmail.com>
2023-02-15 20:05:26 -0300

    criacao da model de eventos

---
+++ agenda/core/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from core.models import Evento
+
+# Altera forma como aparece no admin
+class EventoAdmin(admin.ModelAdmin):
+    list_display = ('titulo','data_evento', 'data_criacao')
+    list_filter = ('usuario', 'data_evento',)
+# Register your models here.
+
+admin.site.register(Evento, EventoAdmin)
