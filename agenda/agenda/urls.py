2284b4feace39b2f582670a75661202482ad0a0e
Luíza <luizaesteves987@gmail.com>
2023-03-08 17:02:12 -0300

    insercao de dados

--- agenda/agenda/urls.py
+++ agenda/agenda/urls.py
@@ -21,8 +21,9 @@ from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('eventos/', include('core.urls')),
     path('agenda/', views.lista_eventos),
+    path('agenda/evento/', views.evento),
+    path('agenda/evento/submit', views.submit_evento),
     path('', RedirectView.as_view(url='/agenda/')),
     path('login/', views.login_user),
     path('login/submit', views.submit_login),
